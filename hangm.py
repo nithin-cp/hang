c34eab8bb52b95818d16cbb874f7f6123a49b559
nithin-cp <nithin.cp@hotmail.com>
2015-11-13 15:58:12 +0530

    code added to the exsiting hangman code

--- hangm.py
+++ hangm.py
@@ -6,16 +6,23 @@ def get_wordlist():
     for i in f:
         i = i.strip()
         if i.isalpha() and len(i)>5:
-            clean_words.append(i, lower())
-        return clean_words
+            clean_words.append(i.lower())
+    return clean_words
 
 def select_word(word_list):
     return random.sample(wordlist,1)[0]
 
-def play_hangman(secret_word):
-
+def play_hangman(a):
+            for letters in a:
+            if letters in gue:
+                print letters
+            else:
+                print"*"
+        if gue not in a:
+            print" wrong guess"
 
 
 wordlist = get_wordlist()
 secret = select_word(wordlist)
-play_hangman(secret)
+play_hangman('cheese')
+
